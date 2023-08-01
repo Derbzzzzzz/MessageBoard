d633e69c3df2a0dec1e32045445311877e4e81b8
Ryan Wahle <ryanwahle8@gmail.com>
2023-08-01 16:51:18 -0500

    test commit

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,16 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
